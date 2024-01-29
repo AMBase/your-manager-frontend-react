05077e7b6a498589fe769d4ee55e2208133bd53f
AMBase <milnikovalexei@gmail.com>
2024-01-29 11:24:32 +0300

    chore: setup webpack

--- bundlers/webpack/webpack.config.ts
+++ bundlers/webpack/webpack.config.ts
@@ -1,13 +1,43 @@
 import path from 'path';
-import webpack from 'webpack';
+import type { Configuration } from "webpack";
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
 
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-const config: webpack.Configuration = {
-    entry: './src/index.ts',
+const devServer: DevServerConfiguration = {
+    static: './dist_webpack',
+};
+
+const config: Configuration = {
+    mode: 'development',
+    entry: './src/index.tsx',
     output: {
         path: path.resolve(__dirname, '../../dist_webpack/assets'),
         filename: '[name]-[chunkhash].js',
+        clean: true,
     },
+    devServer,
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: ['.ts', '.tsx', '.js'],
+        // Add support for TypeScripts fully qualified ESM imports.
+        extensionAlias: {
+            '.js': ['.js', '.ts'],
+            '.cjs': ['.cjs', '.cts'],
+            '.mjs': ['.mjs', '.mts']
+        }
+    },
+    module: {
+        rules: [
+            // all files with a `.ts`, `.cts`, `.mts` or `.tsx` extension will be handled by `ts-loader`
+            { test: /\.([cm]?ts|tsx)$/, loader: 'ts-loader' },
+            {
+                test: /\.svg$/i,
+                type: 'asset/resource'
+            },
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin()],
 };
 
 export default config;
