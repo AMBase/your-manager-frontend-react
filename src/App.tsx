42413e628763fc5abeb78be9ef3ae819747791f6
AMBase <milnikovalexei@gmail.com>
2024-01-30 22:05:32 +0300

    feat: move area

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
+
 // import logo from './logo.svg';
 
 function App(): React.JSX.Element {
-  const onMouseUp = e => {
-    console.log("onMouseUp", e);
+  const [isMoving, setIsMoving] = useState(false);
+  const [prevPos, setPrevPos] = useState({x: 0, y: 0});
+  const [translate, setTranslate] = useState({x: 0, y: 0});
+  const onMouseUp = () => {
+    setIsMoving(false);
   }
 
   const onMouseMove = e => {
-    console.log("onMouseMove", e);
+    if (isMoving) {
+      let deltaX = e.clientX - prevPos.x;
+      let deltaY = e.clientY - prevPos.y;
+
+      setTranslate({
+        x: translate.x + deltaX,
+        y: translate.y + deltaY,
+      });
+      setPrevPos({x: e.clientX, y: e.clientY});
+    }
   }
 
   const onMouseDown = e => {
-    console.log("onMouseDown", e);
+    setIsMoving(true);
+    setPrevPos({x: e.clientX, y: e.clientY});
+  }
+
+  const styles = {
+    transform: `translate(${translate.x}px, ${translate.y}px)`
   }
 
   return (
     <div className="app">
       <div className="wrap">
         <div className="inner"
+             style={styles}
              onMouseUp={onMouseUp}
              onMouseMove={onMouseMove}
              onMouseDown={onMouseDown}>
