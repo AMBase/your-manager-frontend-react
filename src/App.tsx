438cbcfe8953e15c10ca5065aa2cd1e702c72078
AMBase <milnikovalexei@gmail.com>
2024-02-12 22:56:40 +0300

    refactor: move events to wrap

--- src/App.tsx
+++ src/App.tsx
@@ -23,71 +23,29 @@ const actions: Actions = [
 ];
 
 function App(): React.JSX.Element {
-    const [isMoving, setIsMoving] = useState(false);
-    const [prevPos, setPrevPos] = useState({x: 0, y: 0});
-    const [translate, setTranslate] = useState({x: 0, y: 0});
-    const [scale, setScale] = useState(1);
-    const onMouseUp = () => {
-        setIsMoving(false);
+    const [items, setItems] = useState([]);
+
+    const onMouseDown = (e: React.MouseEvent)  => {
+        console.log(e);
     }
-
-    const onMouseMove: React.MouseEventHandler = (e: React.MouseEvent) => {
-        if (isMoving) {
-            let deltaX = e.clientX - prevPos.x;
-            let deltaY = e.clientY - prevPos.y;
-
-            setTranslate({
-                x: translate.x + deltaX,
-                y: translate.y + deltaY,
-            });
-            setPrevPos({x: e.clientX, y: e.clientY});
-        }
-    }
-
-    const onMouseDown: React.MouseEventHandler = (e: React.MouseEvent) => {
-        setIsMoving(true);
-        setPrevPos({x: e.clientX, y: e.clientY});
-    }
-
-    const onWheel: React.WheelEventHandler = (e: React.WheelEvent) => {
-        if (!e.ctrlKey) {
-            return;
-        }
-
-        e.preventDefault();
-        e.deltaY < 0 ? zoomIn() : zoomOut();
-    }
-
-    const zoomIn = () => {
-        if (scale > 1.5) {
-            return;
-        }
-
-        setScale(scale + 0.1);
-    }
-
-    const zoomOut = () => {
-        if (scale < 0.6) {
-            return;
-        }
-
-        setScale(scale - 0.1);
-    }
-
 
     const actionList = actions.map(action =>
-        <div className="action" key={action.id}>
+        <div className="action" key={action.id} onMouseDown={onMouseDown}>
           <div className="action-name">{action.name}</div>
         </div>
     );
 
-    const wrapProps = { scale, translate, onMouseUp, onMouseMove, onMouseDown, onWheel}
+    const itemList = items.map(item =>
+        <div className="item" key={item.id}>
+            <div className="item-name">{item.name}</div>
+        </div>
+    );
 
     return (
         <div className="app">
             <div className="actions">{actionList}</div>
 
-            <Wrap {...wrapProps}></Wrap>
+            <Wrap>{itemList}</Wrap>
         </div>
     );
 }
--- src/Wrap/Wrap.tsx
+++ src/Wrap/Wrap.tsx
@@ -1,21 +1,63 @@
-import React, { FC } from 'react';
+import React, {FC, useState} from 'react';
 
 
 interface WrapProps {
-    scale: number,
-    translate: {
-        x: number,
-        y: number,
-    },
-    onMouseUp: React.MouseEventHandler,
-    onMouseMove: React.MouseEventHandler,
-    onMouseDown: React.MouseEventHandler,
-    onWheel: React.WheelEventHandler,
     children?: React.ReactNode,
 }
 
 const Wrap: FC<WrapProps> = (props: WrapProps) => {
-    const { translate, scale, ...events } = props;
+    const [isMoving, setIsMoving] = useState(false);
+    const [prevPos, setPrevPos] = useState({x: 0, y: 0});
+    const [translate, setTranslate] = useState({x: 0, y: 0});
+    const [scale, setScale] = useState(1);
+    const onMouseUp = () => {
+        setIsMoving(false);
+    }
+
+    const onMouseMove: React.MouseEventHandler = (e: React.MouseEvent) => {
+        if (isMoving) {
+            let deltaX = e.clientX - prevPos.x;
+            let deltaY = e.clientY - prevPos.y;
+
+            setTranslate({
+                x: translate.x + deltaX,
+                y: translate.y + deltaY,
+            });
+            setPrevPos({x: e.clientX, y: e.clientY});
+        }
+    }
+
+    const onMouseDown: React.MouseEventHandler = (e: React.MouseEvent) => {
+        setIsMoving(true);
+        setPrevPos({x: e.clientX, y: e.clientY});
+    }
+
+    const onWheel: React.WheelEventHandler = (e: React.WheelEvent) => {
+        if (!e.ctrlKey) {
+            return;
+        }
+
+        e.preventDefault();
+        e.deltaY < 0 ? zoomIn() : zoomOut();
+    }
+
+    const zoomIn = () => {
+        if (scale > 1.5) {
+            return;
+        }
+
+        setScale(scale + 0.1);
+    }
+
+    const zoomOut = () => {
+        if (scale < 0.6) {
+            return;
+        }
+
+        setScale(scale - 0.1);
+    }
+
+    const events = { onMouseDown, onMouseMove, onMouseUp, onWheel }
     const styles = {
         transform: `translate(${translate.x}px, ${translate.y}px) scale(${scale})`
     }
